e0dca38b7afd9ee69ecff9f14430b33f9d377cc9
Kuljot Biring <ksbiring1@gmail.com>
2023-06-14 23:01:51 -0700

    add queries for student database

---
+++ Section 3/student_queries.sql
@@ -0,0 +1,10 @@
+/*
+* Alright so we just saw how queries are made
+* Try to complete the following query by adding in the appropriate identifier where we have added comments
+*/
+
+SELECT id, firstname, lastname/* get the students id and name here */
+FROM Student;
+
+SELECT * /* get all of students info here */
+FROM Student;
