4f2fd36412eb17237d2ebbe48d493031d13a535f
Kuljot Biring <ksbiring1@gmail.com>
2023-06-17 09:23:12 -0700

    add simple queries to repo

---
+++ Section 5/simplequeries.sql
@@ -0,0 +1,13 @@
+-- Get all the employees
+SELECT * FROM "employees";
+
+-- Check how many departments there are
+SELECT * FROM "departments";
+
+-- How many times has employee 1001 had a raise
+SELECT * FROM "salaries";
+
+-- What title does 1006 have
+SELECT * FROM "titles";
+
+
