f95ec72946313b7f9fe11a00515c936bca2aae42
Kuljot Biring <ksbiring1@gmail.com>
2023-06-30 16:53:00 -0700

    add queries using group by

---
+++ Section 6/groupby.sql
@@ -0,0 +1,38 @@
+/*
+*  How many people were hired on any given hire date?
+*  Database: Employees
+*  Table: Employees
+*/
+
+select hire_date, count(emp_no) as "amount"
+from employees
+group by hire_date
+ORDER by "amount" DESC;
+
+/*
+*   Show me all the employees, hired after 1991 and count the amount of positions 
+they've had
+*  Database: Employees
+*/
+
+SELECT e.emp_no, count(t.title) as "amount of titles"
+from employees as e 
+join titles as t on e.emp_no = t.emp_no
+where EXTRACT (Year from e.hire_date) > 1991
+group by e.emp_no
+ORDER by e.emp_no ASC;
+
+
+
+/*
+*  Show me all the employees that work in the department development and the from and 
+to date.
+*  Database: Employees
+*/
+
+SELECT e.emp_no, de.from_date, de.to_date
+FROM employees as e
+JOIN dept_emp AS de USING(emp_no)
+WHERE de.dept_no = 'd005'
+GROUP BY e.emp_no, de.from_date, de.to_date
+ORDER BY e.emp_no, de.to_date;
